bef86757a169e7eb6d6e02ff82ee330fe4b156b9
Mahishwara <kmisha043@mail.ru>
2024-01-22 13:19:03 +0500

    Create project

---
+++ API/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+class Profession(models.Model):
+    name = models.CharField(max_length=100)
+
+class Salary(models.Model):
+    year = models.IntegerField()
+    value = models.FloatField()
+    profession = models.ForeignKey(Profession, on_delete=models.CASCADE)
+
+class Vacancy(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    skills = models.CharField(max_length=200)
+    company = models.CharField(max_length=200)
+    salary = models.FloatField()
+    region = models.CharField(max_length=100)
+    published_date = models.DateField()
+    profession = models.ForeignKey(Profession, on_delete=models.CASCADE)
